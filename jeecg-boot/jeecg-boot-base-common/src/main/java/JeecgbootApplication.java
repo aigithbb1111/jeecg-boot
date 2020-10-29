48250bbe8c4ca118e2d854a60b991a26f411f5d2
jshuangbingbing <huangbb11@chinaunicom.cn>
2020-10-29 16:09:21 +0800

    测试boot

---
+++ jeecg-boot/jeecg-boot-base-common/src/main/java/JeecgbootApplication.java
@@ -0,0 +1,10 @@
+/**
+ * @Author:huangbingbing
+ * @Date：Created on 2020/10/29 16:03
+ * Description:
+ */
+public class JeecgbootApplication {
+    {
+        //System.out.println("test");
+    }
+}
---
+++ jeecg-boot/jeecg-boot-base-common/src/main/java/boot.java
@@ -0,0 +1,10 @@
+/**
+ * @Author:huangbingbing
+ * @Date：Created on 2020/10/29 16:05
+ * Description:
+ */
+public class boot {
+    void f1(){
+        
+    }
+}
